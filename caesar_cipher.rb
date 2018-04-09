4c615d32f8442b72c7acec1285198ee1424f2fa5
Scott McKell <scott.g.mckell@gmail.com>
2018-04-08 23:14:30 -0600

    heroku stuff

---
+++ caesar_cipher.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do 
+	def caesar_cipher(string, rot=1)
+		output = string.split("")
+		output.map! do |x|
+			if x.ord.between?("A".ord, "Z".ord)
+				x = x.ord + rot <= "Z".ord ? (x.ord + rot).chr : (x.ord + rot - 26).chr
+			elsif x.ord.between?("a".ord, "z".ord)
+				x = x.ord + rot <= "z".ord ? (x.ord + rot).chr : (x.ord + rot - 26).chr
+			else
+				x
+			end
+		end
+		output.join("")
+	end
+	phrase = caesar_cipher(params["phrase"], params["rotations"].to_i)
+	erb :index, :locals => {:phrase => phrase}
+end
